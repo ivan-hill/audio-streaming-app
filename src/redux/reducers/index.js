92a6d0f2fff9c99e323ebee25576344d08e3aa35
Ivan Hill <30849044+ivan-hill@users.noreply.github.com>
2024-09-09 19:03:57 -0400

    Delete src directory

--- src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-import audioReducer from './audioReducer'; 
-
-const rootReducer = combineReducers({
-  audio: audioReducer,
-  
-});
-
-export default rootReducer;
